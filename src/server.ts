55652bee2df428120ad724d3e85cf50457f21bc0
Erick Juan <68560717+erickjuan50@users.noreply.github.com>
2026-04-14 14:15:23 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,5 @@ const PORT = ENV.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`[Server] API is running on port ${PORT}`);
   console.log(`[Server] Core HTTP Client initialized with external URL: ${ENV.API_URL}`);
+  console.log(`[Server] API-Documentation in 192.168.0.18:4313/api-docs`)
 });
